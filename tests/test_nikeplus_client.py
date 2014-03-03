924386231963d5183797d7fbf097eb2c53d1bb90
vestel <vestel@blog.copperred.net>
2014-03-03 17:18:08 +0100

    Fixed test dependency import (empties)

--- tests/test_nikeplus_client.py
+++ tests/test_nikeplus_client.py
@@ -1,5 +1,5 @@
 import unittest
-import nikeplus2tcx.lib.nikeplus_client
+import lib.nikeplus_client
 
 class NikeplusClientTest(unittest.TestCase):
     def test_dummy(self):
--- tests/test_tcx_builder.py
+++ tests/test_tcx_builder.py
@@ -1,5 +1,5 @@
 import unittest
-import nikeplus2tcx.lib.tcx_builder
+import lib.tcx_builder
 
 class TCXBuilderTests(unittest.TestCase):
     def test_empty(self):
